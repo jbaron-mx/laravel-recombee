ab6e1367190f3515e92866ccd2c4972a420f37ad
Jesus Baron <jbaron@outlook.com>
2022-07-12 01:02:38 -0500

    Additional testing of import command options

--- tests/Feature/Commands/ImportCommandTest.php
+++ tests/Feature/Commands/ImportCommandTest.php
@@ -59,3 +59,60 @@ it('can run import command', function () {
 
     $this->artisan('recombee:import')->assertExitCode(0);
 });
+
+it('can run import command only users', function () {
+    $userModels = User::factory()->count(3)->create();
+
+    $userProperties = [
+        new AddUserProperty('name', 'string'),
+        new AddUserProperty('active', 'boolean'),
+    ];
+    $users = $userModels->map(
+        fn ($model) => new SetUserValues($model->id, Arr::except($model->toArray(), 'id'), ['cascadeCreate' => true])
+    )->all();
+
+    $mock = $this->mock(Client::class);
+
+    $mock->shouldReceive('send')
+        ->ordered()
+        ->once()
+        ->with(Matchers::equalTo(new Batch($userProperties)))
+        ->andReturn([['code' => 201, 'json' => 'ok']]);
+
+    $mock->shouldReceive('send')
+        ->ordered()
+        ->once()
+        ->with(Matchers::equalTo(new Batch($users)))
+        ->andReturn([['code' => 201, 'json' => 'ok']]);
+
+    $this->artisan('recombee:import -u')->assertExitCode(0);
+});
+
+it('can run import command only items', function () {
+    $itemModels = Item::factory()->count(3)->create();
+
+    $itemProperties = [
+        new AddItemProperty('name', 'string'),
+        new AddItemProperty('price', 'double'),
+        new AddItemProperty('active', 'boolean'),
+    ];
+    $items = $itemModels->map(
+        fn ($model) => new SetItemValues($model->id, Arr::except($model->toArray(), 'id'), ['cascadeCreate' => true])
+    )->all();
+
+    $mock = $this->mock(Client::class);
+
+    $mock->shouldReceive('send')
+        ->ordered()
+        ->once()
+        ->with(Matchers::equalTo(new Batch($itemProperties)))
+        ->andReturn([['code' => 201, 'json' => 'ok']]);
+
+    $mock->shouldReceive('send')
+        ->ordered()
+        ->once()
+        ->with(Matchers::equalTo(new Batch($items)))
+        ->andReturn([['code' => 201, 'json' => 'ok']]);
+
+    $this->artisan('recombee:import -i')->assertExitCode(0);
+});
